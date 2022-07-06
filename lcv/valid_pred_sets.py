61db72f30ea7efddf0c6300cedcb4fc726b31c74
Monoxido45 <lucruz45.cab@gmail.com>
2022-07-06 10:10:37 -0300

    More simulated experiments added and some modifications made to hyp test

--- lcv/valid_pred_sets.py
+++ lcv/valid_pred_sets.py
@@ -33,28 +33,31 @@ class Valid_pred_sets(BaseEstimator):
         self.alpha = alpha
         
     
-    def fit(self, X_calib, y_calib, random_seed = None, test_size = 0.2):
+    def fit(self, X_calib, y_calib, random_seed = 1250, test_size = 0.2, **kwargs):
         # predicting each interval
         preds = self.conf.predict(X_calib, significance = self.alpha)
-        
+        np.random.seed(random_seed)
         # obtaining each w
         w = np.zeros(y_calib.shape[0])
         for i in range(y_calib.shape[0]):
             w[i] = int(y_calib[i] >= preds[i, 0] and y_calib[i] <= preds[i, 1])
         # splitting training and testing sets
         self.X_train, self.X_test, self.w_train, self.w_test = train_test_split(X_calib,w,
-                                                                                test_size = test_size,
-                                                                                random_state = random_seed)
-
+                                                                                test_size = test_size)
+        
+        # regressing w on x using random forest model
+        self.model = RandomForestClassifier(**kwargs).fit(self.X_train, self.w_train)
         return self
     
     def check(self, i):
         return "Iteracao %d" %i
     
-    def monte_carlo_test(self, B = 1000, random_seed = 1250, **kwargs):
-        # regressing w on x using random forest model
-        self.model = RandomForestClassifier(**kwargs).fit(self.X_train, self.w_train)
-        
+    def r_prob(self, X_grid):
+        # predicting for each x in X_grid
+        r = self.model.predict_proba(X_grid)[:, 1]
+        return r
+    
+    def monte_carlo_test(self, B = 1000, random_seed = 1250, **kwargs):      
         # observed statistic
         r = self.model.predict_proba(self.X_test)[:, 1]
         t_obs = np.mean(np.abs(r  - (1 - self.alpha)))
@@ -84,10 +87,10 @@ class Valid_pred_sets(BaseEstimator):
 class LinearQuantileRegression(BaseEstimator):
     def __init__(self, 
                  coverage = 0.05, 
-                 alpha = 1, 
+                 alpha = 0, 
                   fit_intercept = True, 
-                  solver = 'interior-point', 
-                  solver_options=None):
+                  solver = 'highs', 
+                  solver_options = None):
         for prop in dir():
             if prop != "self":
                 setattr(self, prop, locals()[prop])
